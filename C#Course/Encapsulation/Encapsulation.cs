667b883ef5ef94864b097e6c27bed31d2d732fca
Jonatan <jonatanav255@hotmail.com>
2024-09-28 20:17:04 -0600

    c# example
    java deleted

---
+++ C#Course/Encapsulation/Encapsulation.cs
@@ -0,0 +1,38 @@
+public class BadBankAccount
+{
+    private decimal balance;
+
+    public BadBankAccount(decimal balance)
+    {
+        Deposit(balance);
+    }
+
+    public decimal GetBalance()
+    {
+        return balance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Deposit amount must be positive");
+        }
+
+        this.balance += amount;
+    }
+
+    public void Withdraw(decimal amount){
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Withdrawal amount must be positive");
+        }
+
+        if (amount > balance)
+        {
+            throw new InvalidOperationException("Insufficient funds");
+        }
+
+        this.balance -= amount;
+    }
+}
---
+++ C#Course/Program.cs
@@ -0,0 +1,11 @@
+﻿BadBankAccount badBankAccount = new BadBankAccount(1000);
+
+System.Console.WriteLine(badBankAccount.GetBalance());
+badBankAccount.Deposit(100);
+System.Console.WriteLine(badBankAccount.GetBalance());
+badBankAccount.Withdraw(400);
+System.Console.WriteLine(badBankAccount.GetBalance());
+
+// badBankAccount.balance = 100;
+
+// System.Console.WriteLine(badBankAccount.balance) ;
